d5f908435665c3d0a059ff071821e037855793f4
Avenged <43619042+Avenged2@users.noreply.github.com>
2019-05-11 20:33:05 +0200

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-    $JSON_URL = "https://discordapp.com/api/guilds/524243316565475339/widget.json";
+    $JSON_URL = "Your JSON API";
     $JSON = file_get_contents($JSON_URL);
     $info = json_decode($JSON, true)['members'];
         
